12f1597aba828496441e3401e4ee85286b42a3d4
root <root@DESKTOP-PH29EBJ>
2024-07-25 15:18:16 +0800

    添加usb相关功能, 编译不通过

--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Jan 30 2024"
-#define U_BOOT_TIME "20:42:05"
+#define U_BOOT_DATE "Jul 25 2024"
+#define U_BOOT_TIME "14:56:51"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "01/30/2024"
+#define U_BOOT_DMI_DATE "07/25/2024"
