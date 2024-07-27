fa65512577322ffb898778aaf4258f0231c827b9
TOTHTOT <37585883+TOTHTOT@users.noreply.github.com>
2024-07-27 08:57:51 +0800

    添加功能
    1. 编译脚本支持参数写入tf卡;

--- build_uboot.sh
+++ build_uboot.sh
@@ -3,14 +3,21 @@
  # @Description: uboot 的编译脚本
  # @Author: TOTHTOT
  # @Date: 2024-01-27 11:46:41
- # @LastEditTime: 2024-01-30 20:43:53
+ # @LastEditTime: 2024-07-26 23:09:08
  # @LastEditors: TOTHTOT
  # @FilePath: /aw_v3s_uboot/build_uboot.sh
 ### 
 
 # 需要配置的变量
-SD_CARD_PATH="/dev/sdb"
 DEFCONFIG_NAME="yyh_LicheePi_Zero_defconfig"
+
+SD_CARD_PATH=$1
+
+# 检查是否设置了 SD_CARD_PATH 变量并且不为空
+if [ -z "$SD_CARD_PATH" ]; then
+  echo "Error: SD_CARD_PATH is not set or is empty. Aborting."
+  exit 1
+fi
 
 # 检测是否存在/dev/sdb设备
 if [ -e  $SD_CARD_PATH ]; then
@@ -29,6 +36,8 @@ if [ $? -eq 0 ]; then
   # make成功，执行下一步操作
   echo "Make successful. Flashing U-boot to ${SD_CARD_PATH}..."
   sudo dd if=u-boot-sunxi-with-spl.bin of=${SD_CARD_PATH} bs=1024 seek=8
+  # 確保寫入成功
+  sync
 else
   # make失败，输出错误信息并退出
   echo "Make failed. Error encountered."
--- include/generated/timestamp_autogenerated.h
+++ include/generated/timestamp_autogenerated.h
@@ -1,4 +1,4 @@
-#define U_BOOT_DATE "Jul 25 2024"
-#define U_BOOT_TIME "14:56:51"
+#define U_BOOT_DATE "Jul 26 2024"
+#define U_BOOT_TIME "23:31:24"
 #define U_BOOT_TZ "+0800"
-#define U_BOOT_DMI_DATE "07/25/2024"
+#define U_BOOT_DMI_DATE "07/26/2024"
